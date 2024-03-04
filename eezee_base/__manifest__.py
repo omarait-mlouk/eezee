98f07334c746de543ec1131fb14e075d00ad3b0d
Ait-Mlouk Omar <omaraitmlouk.per@gmail.com>
2024-03-04 18:09:46 +0100

    [ADD] add documentation file .pdf

--- eezee_base/__manifest__.py
+++ eezee_base/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Eezee test",
     'author': "Ait-Mlouk Omar",
-    'depends': ['base', 'sale'],
+    'depends': ['base', 'sale_management'],
     'data': [
         'security/sale_order_security.xml',
         'views/sale_order_views.xml',
